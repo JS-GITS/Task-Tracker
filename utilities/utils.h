be5006d9b3924d695c1a6277e6ae06da831e28b6
Justin <justinesiananda@gmail.com>
2025-07-19 02:10:10 +1000

    Added comments in utils.h

--- utilities/utils.h
+++ utilities/utils.h
@@ -5,10 +5,9 @@
 #include <vector>
 #include "CLI.h"
 using namespace std;
-// get_date is a function that returns the current date
-string get_date();
-void write_to_file(ofstream &file,const vector<Task> &objects);
-void read_from_file(ifstream &file, vector<Task> &objects);
-void writeJSON(vector<Task> &taskVector);
-void readJSON(vector<Task> &taskVector);
+string get_date();                                                      // Returns the date in DD:m:YY-H:M:S format
+void write_to_file(ofstream &file,const vector<Task> &objects);         // Takes the output file stream and writes the tasks from the vector to the file 
+void read_from_file(ifstream &file, vector<Task> &objects);             // Reads the input file stream and assign the variables to a task and put them in a vector
+void writeJSON(vector<Task> &taskVector);                               // Just faster function to execute writing to a file
+void readJSON(vector<Task> &taskVector);                                // Just faster function to execute reading from a file
 #endif
